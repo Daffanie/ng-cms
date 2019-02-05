b71fb6c217707829bd8d6a7e20cd88f45ae97e2c
Daffanie Hurley <daffhur50@gmail.com>
2019-02-05 16:15:37 -0600

    Implemented an article service

---
+++ src/app/articles.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Article } from './article';
+
+@Injectable()
+export class ArticlesService {
+  private url: string = 'http://loc.mean.example.com:3000/api/articles';
+
+  constructor(private http: HttpClient) { }
+
+  getArticle(): Observable<Article> {
+    return this.http.get<Article>(this.url);
+  }
+
+  getArticles(id: string): Observable<Article> {
+    return this.http.get<Article>(`${this.url}/${id}`);
+  }
+}

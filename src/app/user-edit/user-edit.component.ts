c1d25f776667bed290aa0729c6d4651e62b93009
Daffanie Hurley <daffhur50@gmail.com>
2019-02-05 15:34:18 -0600

    Added a user edit, delete, and Article schema

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 

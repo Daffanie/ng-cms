78d51f5e2594d96e7bba200ed8080dcbd97bcbaa
Daffanie Hurley <daffhur50@gmail.com>
2019-02-05 11:22:34 -0600

    Implemented a user view

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -22,4 +22,8 @@ export class UsersService{
     return this.http.get<User>(this.url);
   }
 
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.url}/${id}`);
+  }
+
 }
